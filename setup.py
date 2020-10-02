91d1eb18ea51fccb596dc9ffe828214d2bec7aa0
azhar22k <azhar.khan.78601@gmail.com>
2020-10-02 15:34:04 +0530

    Documention fixes and Github action Workflow

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='ais_service_discovery',
-    version='0.1.0',
+    version='0.1.1',
     author='Peak AI',
     author_email='infra-notifications@peak.ai',
     description='AIS service discovery package for python3',
